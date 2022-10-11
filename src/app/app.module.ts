433bbabc43ba7575cda24f2021180afbc7d81a8a
Varshini <varshuk01@gmail.com>
2022-10-11 21:27:35 +0530

    userdata

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { UserdataComponent } from './userdata/userdata.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    UserdataComponent
   ],
   imports: [
     BrowserModule,
